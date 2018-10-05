222b845db3237907dabd5102a804e2d8592f90b3
Kakeru Nemoto <drumgadaisuki@gmail.com>
2018-10-05 14:16:53 +0900

    Initial Commit

---
+++ Calender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calender
+//
+//  Created by 根本翔 on 2018/10/05.
+//  Copyright © 2018年 根本翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CalenderTests/CalenderTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalenderTests.swift
+//  CalenderTests
+//
+//  Created by 根本翔 on 2018/10/05.
+//  Copyright © 2018年 根本翔. All rights reserved.
+//
+
+import XCTest
+@testable import Calender
+
+class CalenderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
